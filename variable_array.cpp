5da57748fe5f61f98aa18b5170798375ebc9fa89
arxer4359 <47049865+arxer4359@users.noreply.github.com>
2021-06-07 10:40:05 +0530

    Update variable_array.cpp

--- variable_array.cpp
+++ variable_array.cpp
@@ -6,8 +6,7 @@
 using namespace std;
 
 
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+int main() { 
     int n,q,k,i,j;
     cin >> n >> q;
     vector<vector<int> >arr(n);
