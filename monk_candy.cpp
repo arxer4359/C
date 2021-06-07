79b17c42d2c0adb21ffbf529429ac4852c656c09
arxer4359 <47049865+arxer4359@users.noreply.github.com>
2021-06-07 10:14:32 +0530

    Create monk_candy.cpp

---
+++ monk_candy.cpp
@@ -0,0 +1,52 @@
+#include<iostream.h>
+
+using namespace std;
+
+int main(){
+
+    int t;
+
+    cin>>t;
+
+    while(t--){
+
+        int n,k;
+
+        cin>>n>>k;
+
+        multiset<long long> s;
+
+        for(int i=0;i<n;i++){
+
+            long long l;
+
+            cin>>l;
+
+            s.insert(l);
+
+
+ 
+
+        }
+
+        long long sum=0;
+
+        
+
+        while(k--){
+
+            long long p=*(--s.end());
+
+            s.erase(s.find(p));
+
+            s.insert(p/2);
+
+            sum+=p;
+
+        }
+
+        cout<<sum<<endl;
+
+    }
+
+}
